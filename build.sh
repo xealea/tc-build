575f3c95496fdb3dee6a2cee02fb640a3d279bef
ʟᴇᴀ <xealea@proton.me>
2024-07-28 20:40:17 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Function to display messages
+msg() {
+    echo "$1"
+}
+
+# Build LLVM and Binutils
+msg "Building LLVM and Binutils..."
 ./build-llvm.py \
   --assertions \
   --vendor-string "Tea" \
@@ -11,4 +18,22 @@
 
 ./build-binutils.py \
   --targets arm aarch64 x86_64
-  
+
+# Remove unused products
+msg "Removing unused products..."
+rm -rf install/include
+rm -f install/lib/*.a install/lib/*.la
+
+# Strip remaining products
+msg "Stripping remaining products..."
+find install -type f -exec file {} + | \
+grep 'not stripped' | \
+cut -d: -f1 | \
+xargs -r strip
+
+# Set executable rpaths
+msg "Setting library load paths for portability..."
+find install -mindepth 2 -maxdepth 3 -type f -exec file {} + | \
+grep 'ELF .* interpreter' | \
+cut -d: -f1 | \
+xargs -r -I{} patchelf --set-rpath '$ORIGIN/../lib' {}
